144cea8b2ea6463d0def9b86bd0bf4c36637ae7c
Andrea Freeman <afreeman5566@gmail.com>
2021-10-02 18:23:23 -0500

    commit tree 0d85fafd3237c2f11b03ff35c59847e1babe8584

--- index.js
+++ index.js
@@ -1,1 +1,123 @@
 // Add your code here
+
+//test 1
+function submitData(username,userEmail){
+const userdataObject = {
+  method: "POST",
+  headers:{
+    "Content-Type": "application/json",
+    Accept : "application/json",
+  },
+  body: JSON.stringify({
+    name: username,
+    email: userEmail,
+  })
+}
+
+fetch("http://localhost:3000/users", userdataObject)
+.then(function (response) { // returns what we posted
+  return response.json(); // converts back to text from Json
+})
+.then(function (object) {  // the results of the json() is returned 
+  console.log(object);    // tells the computer what to do with the returned data
+})
+}
+
+// test 2
+function submitData(username,userEmail){
+  const userdataObject = {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json",
+      Accept : "application/json",
+    },
+    body: JSON.stringify({
+      name: username,
+      email: userEmail,
+    })
+  }
+  
+  fetch("http://localhost:3000/users", userdataObject)
+  .then(function (response) { // returns what we posted
+    return response.json(); // converts back to text from Json
+  })
+  .then(function (object) { // the results of the json() is returned 
+    
+    document.body.append(object.id);    // tells the computer what to do with the returned data
+  })
+  }
+
+  //test 3
+
+function submitData(username,userEmail){
+  const userdataObject = {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json",
+      Accept : "application/json",
+    },
+    body: JSON.stringify({
+      name: username,
+      email: userEmail,
+    })
+  }
+  
+  fetch("http://localhost:3000/users", userdataObject)
+  .then(function (response) { // returns what we posted
+    return response.json(); // converts back to text from Json
+  })
+  .then(function (object) { // the results of the json() is returned 
+    
+    document.body.append(object.id);    // tells the computer what to do with the returned data
+  })
+  .catch(function (error) {
+    error ("Bad things! Ragnarők!");
+    
+    console.log(error.message);
+    document.body.innerHTML = 'Unauthorized Access'
+    })
+  }
+
+// EXAMPLES
+
+//   const formData = {
+//   dogName: "Bryon",
+//   dogBreed:"Poodle",
+//  }
+
+
+
+// const configurationObject = {
+//   method: "POST",
+//   headers:{
+//     "Content-Type": "application/json",
+//     Accept : "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// }
+
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(function (response) { // returns what we posted
+//   return response.json(); // converts back to text from Json
+// })
+// .then(function (object) {  // the results of the json() is returned 
+//   console.log(object);    // tells the computer what to do with the returned data
+// })
+// .catch(function (error) {   
+//   alert("Bad things! Ragnarők!");
+//   console.log(error.message);
+// })
+
+//Example of the above using an anonymous object  
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// });
